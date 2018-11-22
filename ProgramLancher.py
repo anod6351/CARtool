9721b914d17320d1732e176ea194b42af52d78d2
anod6351 <anna.odelgard@live.com>
2018-11-22 14:39:25 +0100

    Update ProgramLancher.py

--- ProgramLancher.py
+++ ProgramLancher.py
@@ -68,12 +68,12 @@ if args.phred_score==None or not(str(args.phred_score[0])=='all'):
 
 
 	# Deal with the region file being one based at the start position
-	#regions_file = open(args.Regions, 'r')
-	#regions_fixed_file = [] 
-	#for line in regions_file:
-	#	element=line.split('\t')
-	#	regions_fixed_file.append([element[0], int(element[1])-1, element[2], element[3]])
-	#Region_fixed = pybedtools.BedTool(regions_fixed_file)
+	regions_file = open(args.Regions, 'r')
+	regions_fixed_file = [] 
+	for line in regions_file:
+		element=line.split('\t')
+		regions_fixed_file.append([element[0], int(element[1])-1, element[2], element[3]])
+	Region_fixed = pybedtools.BedTool(regions_fixed_file)
 
 
 	# Create bedtool objects
@@ -88,7 +88,7 @@ if args.phred_score==None or not(str(args.phred_score[0])=='all'):
 	# Create per base/amplicon coverage
 	print("Creating per base/amplicon coverage ...")
 	import Detailed_generator
-	detailed_list = Detailed_generator.detail(Regions, Reads) ############ Regions_fixed if 1 based start position
+	detailed_list = Detailed_generator.detail(Region_fixed, Reads) ############ Regions_fixed if 1 based start position
 
 
 ########################## Phred score filtering of bam file (OPTIONAL) ################################################################################### 
