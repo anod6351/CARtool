4bfa35d3efe05d5d006ee17abe9fa455e63f9fab
anod6351 <anna.odelgard@live.com>
2018-12-06 11:52:51 +0100

    Update fixedbedfile.py

--- fixedbedfile.py
+++ fixedbedfile.py
@@ -1,11 +1,11 @@
-# To change the start position in a bedfile from 1-based to 0-based if needed. CARtool only takes 0-based start positions
+# To change the end position in a bedfile from 0-based to 1-based if needed. CARtool only takes 0-based start positions and 1 based end positions
 def changestart(filepath):
 
 	with open(filepath, 'r') as regions_file:
 		with open('regions_fixed_file.bed', 'w') as myfile:
 			for line in regions_file:
 				element=line.split('\t')
-				row = str(element[0]) + '\t' + str(int(element[1])-1) + '\t' + str(element[2]) + '\t' + str(element[3])
+				row = str(element[0]) + '\t' + str(element[1]) + '\t' + str(int(element[2])+11) + '\t' + str(element[3])
 				myfile.write(row)
 	regions_file.close()
 	myfile.close()
