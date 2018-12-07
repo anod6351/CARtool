70d5f8d0175bb63c76d689d0796e7341ab30421b
anod6351 <anna.odelgard@live.com>
2018-12-07 13:37:15 +0100

    Update fixedbedfile.py

--- fixedbedfile.py
+++ fixedbedfile.py
@@ -5,8 +5,8 @@ def changestart(filepath):
 		with open('regions_fixed_file.bed', 'w') as myfile:
 			for line in regions_file:
 				element=line.split('\t')
-				row = str(element[0]) + '\t' + str(element[1]) + '\t' + str(int(element[2])+11) + '\t' + str(element[3])
+				row = str(element[0]) + '\t' + str(int(element[1])-1) + '\t' + str(element[2]) + '\t' + str(element[3])
 				myfile.write(row)
 	regions_file.close()
 	myfile.close()
-	return '/regions_fixed_file.bed'
+	
