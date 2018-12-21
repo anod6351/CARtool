69b87d8de3833e65dc1eec2602dd79034302949c
anod6351 <anna.odelgard@live.com>
2018-12-21 08:45:08 +0100

    Update PieChart.py

--- PieChart.py
+++ PieChart.py
@@ -6,19 +6,17 @@ def Generate_Pie(Detailed_list, Threshold, index, fig, title, textSize):
 	import matplotlib
 	matplotlib.use('Agg')
 	import matplotlib.pyplot as plt
-	import Tkinter
+	import tkinter
 
 	count_under = 0
 	count_over = 0
-	print(Detailed_list)
+
 	for element in Detailed_list:
 		if int(element) < int(Threshold):
 			count_under+=1
 		else:
 			count_over+=1
 
-	print(count_over)
-	print(count_under)
 	ax = fig.add_subplot(2,2,index)
 	ax.pie([count_over,count_under], colors=['blue', 'orange']) 
 	ax.axis('equal')
