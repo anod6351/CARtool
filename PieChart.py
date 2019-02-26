53a26bc030636ee132184445c2035fbd8f81fe65
anod6351 <anna.odelgard@live.com>
2019-02-26 09:40:22 +0100

    Update PieChart.py

--- PieChart.py
+++ PieChart.py
@@ -8,6 +8,7 @@ def Generate_Pie(Detailed_list, Threshold, index, fig, title, textSize):
 	import matplotlib.pyplot as plt
 	import tkinter
 
+	# Count values below and over threshold 
 	count_under = 0
 	count_over = 0
 
@@ -17,11 +18,11 @@ def Generate_Pie(Detailed_list, Threshold, index, fig, title, textSize):
 		else:
 			count_over+=1
 
+	# Generate a pie chart with the count over and under threshold. Blue for over and yellow for the values bellow.
 	ax = fig.add_subplot(2,2,index)
 	ax.pie([count_over,count_under], colors=['blue', 'orange']) 
 	ax.axis('equal')
 	ax.set_title(str(title), fontsize=textSize)
-	ax.legend([str(count_over) +' bp. > ' + str(int(Threshold)) +'X', str(count_under) + ' pos. < '+ str(int(Threshold)) + 'X'], loc='lower center', bbox_to_anchor=(0.5, 0), fontsize='xx-small')
+	ax.legend([str(count_over) +' bp. > ' + str(int(Threshold)) +'X', str(count_under) + ' bp. < '+ str(int(Threshold)) + 'X'], loc='lower center', bbox_to_anchor=(0.5, 0), fontsize='xx-small')
 
 	return fig
-
